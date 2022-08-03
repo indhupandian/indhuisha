cd02d6334081de04d0bb018392848708df45947d
indhupandian <indhuselvieee@gmail.com>
2022-08-03 14:09:20 +0530

    message

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,14 @@
+package org.emp;
+
+public class Employee {
+	private void empName() {
+	System.out.println("indhu");
+
+	}
+	public static void main(String[] args) {
+		Employee e=new Employee();
+		e.empName();
+		
+	}
+
+}
